c1330e369020b8bbb97071550ec0c141ce9c2ef9
Mitch Toran <mitchellht34@gmail.com>
2024-05-28 16:37:35 -0400

    add navBar

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-console.log("hello world");
-fetch(`https://opentdb.com/api.php?amount=10`)
+const navBar = document.getElementById("nav-bar");
+const categoryDropdown = document.createElement('select');
+const typeDropdown = document.createElement('select');
+const difficultyDropdown = document.createElement('select');
+navBar.append(categoryDropdown, typeDropdown, difficultyDropdown);
+
+
+function apiFetch(){
+    fetch(`https://opentdb.com/api.php?amount=10`)
     .then((resp) => resp.json())
     .then((data) => console.log(data));
+}
