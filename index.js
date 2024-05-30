3c3a3309bc34dcaab96d9c3c15ae963373412c5a
Mitch Toran <mitchellht34@gmail.com>
2024-05-30 15:41:09 -0400

    correct format for url fetch

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-const navBar = document.getElementById("nav-bar");
-const categoryDropdown = document.createElement('select');
-const typeDropdown = document.createElement('select');
-const difficultyDropdown = document.createElement('select');
-
-
-// navBar.append(categoryDropdown, typeDropdown, difficultyDropdown);
 
 
 
-function apiFetch(){
+let object = {
+    amount: 10,
+    category: 9,
+    difficulty: "easy",
+    type: "boolean"
+}
+console.log(object)
+let string = `https://opentdb.com/api.php?amount=${object.amount}${object.category ? "&category=" + object.category : ""}${object.difficulty ? "&difficulty=" + object.difficulty : ""}${object.type ? "&type=" + object.type : ""}`;
+console.log(string);
+console.log(!!object.category);
+
+
+function apiFetch(urlObject){
+    
     fetch(`https://opentdb.com/api.php?amount=10`)
     .then((resp) => resp.json())
     .then((data) => console.log(data));
