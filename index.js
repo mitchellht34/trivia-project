5ba36bdd2d62e82b19c0754b92b9bbc2b90e958f
Mitch Toran <mitchellht34@gmail.com>
2024-05-30 17:03:51 -0400

    append questions and answer choices to display

--- index.js
+++ index.js
@@ -6,10 +6,13 @@ document.addEventListener("DOMContentLoaded", () => {
     const type = document.getElementById("type");
     const difficulty = document.getElementById("difficulty");
     const form = document.querySelector("form");
+    const questions = document.getElementById("questions");
+    const questionHeader = document.createElement('h2');
+    questionHeader.innerText = "Questions";
 
     form.addEventListener('submit', (event) => {
         event.preventDefault();
-        
+
         const numQuestions = document.getElementById("num-questions");
         const category = document.getElementById("category");
         const type = document.getElementById("type");
@@ -24,14 +27,39 @@ document.addEventListener("DOMContentLoaded", () => {
 
         const url = `https://opentdb.com/api.php?amount=${urlObject.amount}${urlObject.category ? "&category=" + urlObject.category : ""}${urlObject.difficulty ? "&difficulty=" + urlObject.difficulty : ""}${urlObject.type ? "&type=" + urlObject.type : ""}`;
         apiFetch(url);
+        
+        numQuestions.value = '10';
+        category.value = '';
+        type.value = '';
+        difficulty.value = '';
+
+        questions.innerHTML = '';
+        questions.append(questionHeader);
+
     })
 })
 
 function apiFetch(url){
-    
     fetch(url)
     .then((resp) => resp.json())
     .then((data) => {
-        console.log(data.results);
+        data.results.forEach((element) => appendQuestion(element));
     });
+}
+
+function appendQuestion(element){
+    const display = document.getElementById("questions");
+    const question = document.createElement("li");
+    question.innerText = element.question;
+    display.append(question)
+
+    console.log(element);
+    const answers = element.type === 'multiple' ? [element.correct_answer, ...element.incorrect_answers] : ["True", "False"];
+    const answerChoices = document.createElement('ul');
+    question.append(answerChoices);
+    answers.forEach((answer) => {
+        const option = document.createElement('li');
+        option.innerText = answer;
+        answerChoices.appendChild(option);
+    })
 }
